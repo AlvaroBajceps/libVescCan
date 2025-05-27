9f9d3437816bc5a7bad39a936fc01042cd1b57ee
Marcin Walczyk <marcinwal9@gmail.com>
2025-05-27 17:04:34 +0200

    add SetOrigin and SetPosSpeedLoop

--- include/libVescCan/VESC_Consts.h
+++ include/libVescCan/VESC_Consts.h
@@ -5,12 +5,17 @@
 
 //****
 // typedefs
+// for raw can frame layout not structs!
 //****
 
 typedef uint8_t VESC_Id_t;
 typedef uint8_t VESC_Command_t;
 
-typedef uint32_t VESC_CommandData_t;
+typedef uint32_t VESC_Command_commandData_t;
+typedef uint8_t VESC_Command_commandDataExB;
+typedef uint32_t VESC_Command_commandDataExF_0;
+typedef uint16_t VESC_Command_commandDataExF_1;
+typedef uint16_t VESC_Command_commandDataExF_2;
 
 typedef int32_t VESC_Status_1_erpm_t;
 typedef int16_t VESC_Status_1_current_t;
@@ -82,6 +87,8 @@ typedef enum VESC_Command_enum
     VESC_COMMAND_SET_CURRENT_BRAKE = 2,          // arg unit [A]
     VESC_COMMAND_SET_RPM = 3,                    // arg unit [RPM]
     VESC_COMMAND_SET_POS = 4,                    // arg unit [Deg]
+    VESC_COMMAND_SET_ORIGIN = 5,                 // see VESC_SetOrigin_Command_Enum; use commandDataExB
+	VESC_COMMAND_SET_POS_SPEED_LOOP = 6,		 // use commandDataEx_0,1,2
     VESC_COMMAND_SET_CURRENT_REL = 10,           // arg unit [% / 100]
     VESC_COMMAND_SET_CURRENT_BRAKE_REL = 11,     // arg unit [% / 100]
     VESC_COMMAND_SET_CURRENT_HANDBRAKE = 12,     // arg unit [A]
@@ -102,6 +109,12 @@ typedef enum VESC_Command_enum
 	VESC_COMMAND_STATUS_10 = 32
 } VESC_Command;
 
+typedef enum VESC_SetOrigin_Command_Enum
+{
+	VESC_SETORIGIN_COMMAND_TEMP = 0,
+	VESC_SETORIGIN_COMMAND_PERMANENT = 1,
+} VESC_SetOrigin_Command;
+
 
 //****
 // VESC scales
@@ -114,6 +127,9 @@ typedef enum VESC_Command_enum
 #define VESC_SCALE_SET_CURRENT_BRAKE 1000
 #define VESC_SCALE_SET_RPM 1
 #define VESC_SCALE_SET_POS 1000000
+#define VESC_SCALE_SET_POS_SPEED_LOOP_POSITION 10000
+#define VESC_SCALE_SET_POS_SPEED_LOOP_SPEED 10
+#define VESC_SCALE_SET_POS_SPEED_LOOP_ACCELERATION 10
 #define VESC_SCALE_SET_CURRENT_REL 100000
 #define VESC_SCALE_SET_CURRENT_BRAKE_REL 100000
 #define VESC_SCALE_SET_CURRENT_HANDBRAKE 1000
@@ -164,11 +180,18 @@ typedef enum VESC_Command_enum
 // enum for offsets
 //****
 
-enum _VESC_offsetIdx_Status_1_enum
+enum _VESC_offsetIdx_PosSpeedLoop_enum
 {
     _VESC_OFFSETIDX_STATUS_1_ERPM = 0,
     _VESC_OFFSETIDX_STATUS_1_CURRENT,
     _VESC_OFFSETIDX_STATUS_1_DUTYCYCLE
+};
+
+enum _VESC_offsetIdx_Status_1_enum
+{
+    _VESC_OFFSETIDX_POS_SPEED_LOOP_POSITION = 0,
+    _VESC_OFFSETIDX_POS_SPEED_LOOP_SPEED,
+    _VESC_OFFSETIDX_POS_SPEED_LOOP_ACCELERATION
 };
 
 enum _VESC_offsetIdx_Status_2_enum
@@ -240,6 +263,13 @@ enum _VESC_offsetIdx_Status_10_enum
 //****
 
 #define _VESC_OFFSET_COMMANDFRAME 0
+
+static const int _VESC_offset_PosSpeedLoop[] =
+{
+    0,
+    sizeof(VESC_Command_commandDataExF_0),
+    sizeof(VESC_Command_commandDataExF_0) + sizeof(VESC_Command_commandDataExF_1)
+};
 
 static const int _VESC_offset_Status_1[] =
 {
