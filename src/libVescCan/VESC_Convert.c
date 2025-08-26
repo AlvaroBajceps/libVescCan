fc42526bac522841f5e6d1029f993917ec3704a8
Marcin Walczyk <marcinwal9@gmail.com>
2025-08-26 11:04:16 +0200

    update vesc_status_8

--- src/libVescCan/VESC_Convert.c
+++ src/libVescCan/VESC_Convert.c
@@ -160,10 +160,10 @@ bool VESC_convertStatus8ToRaw(VESC_RawFrame* out, const VESC_Status_8* in)
 	out->can_dlc = VESC_CAN_STATUS_8_DLEN;
 
 	_VESC_WriteRawData16(out, _VESC_offset_Status_8[_VESC_OFFSETIDX_STATUS_8_WEIGHT_A], in->weightA, VESC_SCALE_STATUS_8_WEIGHT_A);
-	_VESC_WriteRawData16(out, _VESC_offset_Status_8[_VESC_OFFSETIDX_STATUS_8_DISTANCE], in->distance, VESC_SCALE_STATUS_8_DISTANCE);
-	_VESC_WriteRawData8u(out, _VESC_offset_Status_8[_VESC_OFFSETIDX_STATUS_8_HUMIDITY], in->humidity, VESC_SCALE_STATUS_8_HUMIDITY);
-	_VESC_WriteRawData8u(out, _VESC_offset_Status_8[_VESC_OFFSETIDX_STATUS_8_VIBRATIONS], in->vibrations, VESC_SCALE_NA);
 	_VESC_WriteRawData16(out, _VESC_offset_Status_8[_VESC_OFFSETIDX_STATUS_8_WEIGHT_B], in->weightB, VESC_SCALE_STATUS_8_WEIGHT_B);
+	_VESC_WriteRawData16(out, _VESC_offset_Status_8[_VESC_OFFSETIDX_STATUS_8_WEIGHT_C], in->weightC, VESC_SCALE_STATUS_8_WEIGHT_C);
+	_VESC_WriteRawData8u(out, _VESC_offset_Status_8[_VESC_OFFSETIDX_STATUS_8_PH], in->ph, VESC_SCALE_STATUS_8_PH);
+	_VESC_WriteRawData8u(out, _VESC_offset_Status_8[_VESC_OFFSETIDX_STATUS_8_DISTANCE], in->distance, VESC_SCALE_STATUS_8_DISTANCE);
 
 	return true;
 }
@@ -367,10 +367,10 @@ bool VESC_convertRawToStatus8(VESC_Status_8* out, const VESC_RawFrame* in)
 	out->vescID = in->vescID;
 
 	_VESC_ReadRawData16(out->weightA, in, _VESC_offset_Status_8[_VESC_OFFSETIDX_STATUS_8_WEIGHT_A], VESC_SCALE_STATUS_8_WEIGHT_A,/*none*/);
-	_VESC_ReadRawData16(out->distance, in, _VESC_offset_Status_8[_VESC_OFFSETIDX_STATUS_8_DISTANCE], VESC_SCALE_STATUS_8_DISTANCE,/*none*/);
-	_VESC_ReadRawData8u(out->humidity, in, _VESC_offset_Status_8[_VESC_OFFSETIDX_STATUS_8_HUMIDITY], VESC_SCALE_STATUS_8_HUMIDITY,/*nope*/);
-	_VESC_ReadRawData8u(out->vibrations, in, _VESC_offset_Status_8[_VESC_OFFSETIDX_STATUS_8_VIBRATIONS], VESC_SCALE_STATUS_8_VIBRATIONS,/*nope*/);
 	_VESC_ReadRawData16(out->weightB, in, _VESC_offset_Status_8[_VESC_OFFSETIDX_STATUS_8_WEIGHT_B], VESC_SCALE_STATUS_8_WEIGHT_B,/*none*/);
+	_VESC_ReadRawData16(out->weightC, in, _VESC_offset_Status_8[_VESC_OFFSETIDX_STATUS_8_WEIGHT_C], VESC_SCALE_STATUS_8_WEIGHT_C,/*none*/);
+	_VESC_ReadRawData8u(out->ph, in, _VESC_offset_Status_8[_VESC_OFFSETIDX_STATUS_8_PH], VESC_SCALE_STATUS_8_PH,/*none*/);
+	_VESC_ReadRawData8u(out->distance, in, _VESC_offset_Status_8[_VESC_OFFSETIDX_STATUS_8_DISTANCE], VESC_SCALE_STATUS_8_DISTANCE,/*none*/);
 
 	return true;
 }
